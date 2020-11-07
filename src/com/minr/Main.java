4053a1e9a6c32da0993ca9949fbe3aa65c424e1a
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-11-08 01:12:43 +0700

    Add files via upload

---
+++ src/com/minr/Main.java
@@ -0,0 +1,16 @@
+package com.minr;
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Program cek kepribadian berdasarkan warna Latihan 40
+ *
+ */
+public class Main extends TestKepribadian{
+    public static void main(String[] args) {
+        Main data = new Main();
+        data.setInisialisasi();
+        data.getResult();
+    }
+}
